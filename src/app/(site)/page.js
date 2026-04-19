aa75b6be928fe930b00bb653abd9cf54612ab55d
OmarTalaat221 <omartalaat1276@gmail.com>
2026-04-19 11:26:57 +0200

    collection page

--- src/app/(site)/page.js
+++ src/app/(site)/page.js
@@ -3,22 +3,10 @@ import SpecialOffer from "../../sections/Home/SpecialOffer";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Collections from "../../components/Collections/Collections.jsx";
 import BlogSection from "../../components/Blog/BlogSection.jsx";
-import { useLocale } from "next-intl";
-// import "./globals.css";
 
 export default function HomePage() {
   return (
     <div style={{ overflow: "hidden" }}>
-      {/* <PageBanner
-        mediaSrc="https://res.cloudinary.com/dp7jfs375/video/upload/v1773140082/set-elsham_isx9my.mp4"
-        gradientColor="#023048"
-        height="lg"
-        align="center"
-        overlayStrength={70}
-        parallax={false}
-      >
-        <HeroContent />
-      </PageBanner> */}
       <HeroBanner />
       <SpecialOffer />
       <FeaturedProducts />
--- src/components/AOSProvider.jsx
+++ src/components/AOSProvider.jsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const AOSProvider = ({ children }) => {
+  const pathname = usePathname();
+
   useEffect(() => {
     AOS.init({
       duration: 800,
@@ -15,6 +18,15 @@ const AOSProvider = ({ children }) => {
       mirror: false,
     });
   }, []);
+
+  useEffect(() => {
+    // A slight delay ensures Next.js has painted the new layout before we trigger the smooth scroll to top
+    const timer = setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, [pathname]);
 
   return <>{children}</>;
 };
